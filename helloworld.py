76c3184b0b1a8920cd5d8c0dc78ca889f664d980
Student <student@ent-v321-016.local>
2013-05-08 19:29:40 -0400

    tree

---
+++ helloworld.py
@@ -0,0 +1,3 @@
+s = 'really loud'
+loud = raw_input ('how loud?')
+print('this is ' + s + '!' * int(loud))
---
+++ tree.py
@@ -0,0 +1,15 @@
+"""
+tree.py
+----
+
+Write program that prints out an ascii art tree. 
+
+http://foureyes.github.com/mtec1002/tree.html
+"""
+
+print('   /\ ') 
+print('  /  \  ') 
+print('  /  \ ')
+print(' /    \  ')
+print('  ----  ')
+print('   ||  ')
